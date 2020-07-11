9f0b194b2407c4676178a573c7a40548a9afc9ee
xujian <xujian@ihote.com>
2020-07-11 22:31:46 +0800

    客户端获取到服务端返回的请求行，请求头，请求体 然后根据状态机拼装生成response返回

--- client.js
+++ client.js
@@ -20,7 +20,10 @@ class Requset {
       this.bodyText = Object.keys(this.body)
         .map((key) => `${key}=${this.body[key]}`)
         .join("&");
+    } else {
+      this.bodyText = JSON.stringify(this.body);
     }
+
     this.headers["content-length"] = this.bodyText.length;
   }
   parseHeader() {
@@ -39,23 +42,25 @@ class Requset {
 
   send(connection) {
     return new Promise((resolve, reject) => {
+      const response = new Response();
       if (connection) {
-        console.log("已经有连接");
-
         connection.write(this.toString());
       } else {
-        console.log("没有连接");
         connection = net.createConnection(
           { port: this.port, host: this.host },
           () => {
             // 'connect' 监听器
             console.log("已连接到服务器");
+
             connection.write(this.toString());
           }
         );
       }
       connection.on("data", (data) => {
-        resolve(data.toString());
+        response.recevice(data.toString());
+        if (response.isFinished) {
+          resolve(response.response);
+        }
         connection.end();
       });
       connection.on("error", (error) => {
@@ -68,6 +73,149 @@ class Requset {
     });
   }
 }
+
+class Response {
+  constructor() {
+    this.statusLine = "";
+    this.headers = {};
+    this.headerName = "";
+    this.headerValue = "";
+    this.body = null;
+    // this.CR = String.fromCharCode(13);
+    // this.LF = String.fromCharCode(10);
+    this.WAITTING_STATUS_LINE = 0;
+    this.WAITTING_STATUS_LINE_END = 1;
+    this.WAITTING_HEADER_NAME = 2;
+    this.WAITTING_HEADER_SPACE = 3;
+    this.WAITTING_HEADER_VALUE = 4;
+    this.WAITTING_HEADER_END = 5;
+    this.WAITTING_HEADER_BLOCK_END = 6;
+    this.WAITTING_BODY = 7;
+
+    this.currentStatus = this.WAITTING_STATUS_LINE;
+  }
+  get isFinished() {
+    return this.body && this.body.isFinished;
+  }
+  get response() {
+    this.statusLine.match(/HTTP\/1.1 ([0-9]+) ([\s\S]+)/);
+    return {
+      statusCode: RegExp.$1,
+      statusText: RegExp.$2,
+      header: this.headers,
+      content: this.body.content.join(""),
+    };
+  }
+  recevice(data) {
+    for (let i = 0; i < data.length; i++) {
+      this.receviceChar(data[i]);
+    }
+  }
+  receviceChar(char) {
+    if (this.currentStatus === this.WAITTING_STATUS_LINE) {
+      if (char === "\r") {
+        this.currentStatus = this.WAITTING_STATUS_LINE_END;
+      }
+      // else if (char === "\n") {
+      //   this.currentStatus = this.WAITTING_HEADER_LINE_NAME;
+      // }
+      else {
+        this.statusLine += char;
+      }
+    } else if (this.currentStatus === this.WAITTING_STATUS_LINE_END) {
+      if (char === "\n") {
+        this.currentStatus = this.WAITTING_HEADER_NAME;
+      } else {
+        this.statusLine += char;
+      }
+    } else if (this.currentStatus === this.WAITTING_HEADER_NAME) {
+      if (char === ":") {
+        this.currentStatus = this.WAITTING_HEADER_SPACE;
+      } else if (char === "\r") {
+        this.currentStatus = this.WAITTING_HEADER_BLOCK_END;
+        //if (this.headers["Transfer-Encoding"] === "") {
+        this.body = new ChunkedBodyParse();
+        //}
+      } else {
+        this.headerName += char;
+      }
+    } else if (this.currentStatus === this.WAITTING_HEADER_SPACE) {
+      if (char === " ") {
+        this.currentStatus = this.WAITTING_HEADER_VALUE;
+      }
+    } else if (this.currentStatus === this.WAITTING_HEADER_VALUE) {
+      if (char === "\r") {
+        this.currentStatus = this.WAITTING_HEADER_END;
+        this.headers[this.headerName] = this.headerValue;
+        this.headerName = "";
+        this.headerValue = "";
+      } else {
+        this.headerValue += char;
+      }
+    } else if (this.currentStatus === this.WAITTING_HEADER_END) {
+      if (char === "\n") {
+        this.currentStatus = this.WAITTING_HEADER_NAME;
+      }
+    } else if (this.currentStatus === this.WAITTING_HEADER_BLOCK_END) {
+      if (char === "\n") {
+        this.currentStatus = this.WAITTING_BODY;
+      }
+    } else if (this.currentStatus === this.WAITTING_BODY) {
+      this.body.receviceChar(char);
+    }
+  }
+}
+class ChunkedBodyParse {
+  constructor() {
+    this.WAITTING_LENGTH = 0;
+    this.WAITTING_LENGTH_END = 1;
+    this.READING_TRUNK = 2;
+    this.WAITTING_NEW_LINE = 3;
+    this.WAITTING_NEW_LINE_END = 4;
+    this.length = 0;
+    this.content = [];
+    this.isFinished = false;
+    this.currentStatus = this.WAITTING_LENGTH;
+  }
+
+  receviceChar(char) {
+    //console.log(JSON.stringify(char));
+
+    if (this.currentStatus === this.WAITTING_LENGTH) {
+      if (char === "\r") {
+        if (this.length === 0) {
+          console.log("===============");
+          console.log(this.content);
+
+          this.isFinished = true;
+        }
+        this.currentStatus = this.WAITTING_LENGTH_END;
+      } else {
+        this.length *= 10;
+        this.length += char.charCodeAt(0) - "0".charCodeAt(0);
+      }
+    } else if (this.currentStatus === this.WAITTING_LENGTH_END) {
+      if (char === "\n") {
+        this.currentStatus = this.READING_TRUNK;
+      }
+    } else if (this.currentStatus === this.READING_TRUNK) {
+      if (this.length === 0) {
+        this.currentStatus = this.WAITTING_NEW_LINE;
+      } else {
+        this.content.push(char);
+        this.length--;
+      }
+    } else if (this.currentStatus === this.WAITTING_NEW_LINE) {
+      if (char === "\r") {
+        this.currentStatus = this.WAITTING_NEW_LINE_END;
+      }
+    } else if (this.currentStatus === this.WAITTING_NEW_LINE_END) {
+      if (char === "\n") {
+        this.currentStatus = this.WAITTING_LENGTH;
+      }
+    }
+  }
+}
 !(async function () {
   const request = new Requset({
     method: "GET",
@@ -75,11 +223,13 @@ class Requset {
     host: "localhost",
     path: "/",
     headers: {
-      "Content-Type": "text/plain",
+      "Content-Type": "text/html",
+      "X-Foo": "bar",
+      // "Transfer-Encoding": "chunked",
     },
     body: { name: "xujian" },
   });
   const connection = await request.send();
 
-  console.log(connection);
+  //console.log(connection);
 })();
--- server.js
+++ server.js
@@ -2,15 +2,26 @@ const http = require("http");
 
 http
   .createServer((req, res) => {
-    console.log("请求成功");
+    //console.log("请求成功");
     //解析客户发来请求消息
-    console.log("请求方法:" + req.method);
-    console.log("请求地址:" + req.url);
-    console.log("协议版本:" + req.httpVersion);
-    console.log("请求头部:" + JSON.stringify(req.headers));
-    res.writeHead(200, { "Content-Type": "text/plain" });
+    //console.log("请求方法:" + req.method);
+    //console.log("请求地址:" + req.url);
+    //console.log("协议版本:" + req.httpVersion);
+    //console.log("请求头部:" + JSON.stringify(req.headers));
 
-    res.end("ok");
+    const obj = {};
+    const content = `DOCTYPEht`;
+    req.on("data", (data) => {});
+    req.on("end", () => {
+      console.log("end");
+      res.end(content);
+    });
+    res.writeHead(200, {
+      "Content-Type": "text/html",
+      "Content-Length": content.length,
+      "X-Foo2": "bar2",
+      "Transfer-Encoding": "chunked",
+    });
   })
   .listen(8080, () => {
     console.log("listen 8080");
